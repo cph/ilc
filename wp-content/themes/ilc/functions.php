55afe8d8a8b9c3fa53aa960d9cf3dfa52cb9a7d3
Nate Paul <nate.paul@cph.org>
2016-02-05 12:23:13 -0600

    fixing email signup header

--- wp-content/themes/ilc/functions.php
+++ wp-content/themes/ilc/functions.php
@@ -105,8 +105,8 @@ function ilc_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h4 class="widget-title">',
+		'after_title'   => '</h4>',
 	) );
 	register_sidebar( array(
 		'name'          => esc_html__( 'Above the Footer', 'ilc' ),
@@ -114,6 +114,8 @@ function ilc_widgets_init() {
 		'description'   => '',
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
+		'before_title'  => '<h4 class="widget-title">',
+		'after_title'   => '</h4>',
 	) );
 }
 
