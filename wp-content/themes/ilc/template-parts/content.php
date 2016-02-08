85e0ec0791b6e1fe3c70d3dc5f5b98b5550cddae
Nate Paul <nate.paul@cph.org>
2016-02-08 16:59:50 -0600

    adding custom field to pages - Add a Map

--- wp-content/themes/ilc/template-parts/content.php
+++ wp-content/themes/ilc/template-parts/content.php
@@ -11,6 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<?php the_field('custom_title'); ?>
 		<?php
 			if ( is_single() ) {
 				the_title( '<h1 class="entry-title">', '</h1>' );
