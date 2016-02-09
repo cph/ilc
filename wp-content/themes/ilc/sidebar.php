7e73b6d6b145db235f9a52c6e9e546f6449b2dc8
Nate Paul <nate.paul@cph.org>
2016-02-09 16:48:17 -0600

    moving breadcrumbs into #main

--- wp-content/themes/ilc/sidebar.php
+++ wp-content/themes/ilc/sidebar.php
@@ -11,8 +11,7 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-<?php if ( function_exists('yoast_breadcrumb') ) 
-		{yoast_breadcrumb('<div id="breadcrumbs">','</div>');} ?>
+
 <aside id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
