07bfcf722ed630f0bc07e2fff81723869f8dcd34
Nate Paul <nate.paul@cph.org>
2016-02-09 16:35:49 -0600

    Adding breadcrumbs to sidebar

--- wp-content/themes/ilc/sidebar.php
+++ wp-content/themes/ilc/sidebar.php
@@ -11,7 +11,8 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-
+<?php if ( function_exists('yoast_breadcrumb') ) 
+		{yoast_breadcrumb('<div id="breadcrumbs">','</div>');} ?>
 <aside id="secondary" class="widget-area" role="complementary">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </aside><!-- #secondary -->
