c7b1d0dd4cae7593e2eb600248344f9db1315da0
Nate Paul <nate.paul@cph.org>
2016-02-09 16:00:46 -0600

    layout update with sidebar

--- wp-content/themes/ilc/page.php
+++ wp-content/themes/ilc/page.php
@@ -15,7 +15,7 @@
 get_header(); ?>
 
 	<div id="primary" class="content-page">
-		<main id="main" class="site-main-page" role="main">
+		<main id="main" class="site-main" role="main">
 			<?php
 			while ( have_posts() ) : the_post();
 				
